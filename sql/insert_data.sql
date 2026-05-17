5234d0a452f5910e6c8be0debb5baf6d2005db8e
ZQM2026 <112210111@stu.jp-sji.org>
2026-05-17 22:11:18 +0900

    Update insert_data.sql

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -36,35 +36,3 @@ INSERT INTO employees VALUES
 (28, 'Okada', 31, 'Female', 5, 380000, 23, '2019-02-28'),
 (29, 'Hasegawa', 35, 'Male', 4, 560000, 19, '2017-03-16'),
 (30, 'Murakami', 28, 'Female', 2, 410000, 27, '2021-08-20');
-
-SELECT * FROM employees;
-SELECT
-    d.department_name,
-    ROUND(AVG(e.salary), 2) AS avg_salary
-FROM employees e
-JOIN departments d
-ON e.department_id = d.department_id
-GROUP BY d.department_name
-ORDER BY avg_salary DESC;
-SELECT
-    name,
-    overtime_hours
-FROM employees
-ORDER BY overtime_hours DESC
-LIMIT 5;
-
-SELECT
-    name,
-    department_id,
-    salary,
-    RANK() OVER(
-        PARTITION BY department_id
-        ORDER BY salary DESC
-    ) AS salary_rank
-FROM employees;
-
-SELECT
-    age,
-    salary
-FROM employees
-ORDER BY age;
