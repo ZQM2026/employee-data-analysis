b20a17599b99712ec83aa700e392c52ae08e0b57
ZQM2026 <112210111@stu.jp-sji.org>
2026-05-17 22:04:58 +0900

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -1,19 +1,3 @@
-CREATE TABLE departments (
-    department_id INT PRIMARY KEY,
-    department_name VARCHAR(50)
-);
-CREATE TABLE employees (
-    employee_id INT PRIMARY KEY,
-    name VARCHAR(50),
-    age INT,
-    gender VARCHAR(10),
-    department_id INT,
-    salary INT,
-    overtime_hours INT,
-    hire_date DATE,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(department_id)
-);
 
 INSERT INTO departments VALUES
 (1, 'Sales'),
